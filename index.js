36adb0219dd3c103c85d6e5470f2fc53a3227072
lynnelle <lynnellewanja37@mail.com>
2024-03-19 12:03:39 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1=31
+let num2=2
+let multiply= num1*num2
+let random = Math.floor(Math.random() * 10) + 1;
+let num3=44
+let num4=10
+const mod =num3%num4;
+let max=Math.max(1, 2, 3, 4, 20);
 // Write your code here
